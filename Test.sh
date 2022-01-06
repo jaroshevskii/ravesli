37fa373bf19ffd2cdf5f8e38e692cdbb7147f001
jaroshevskii <jaroshevskiisasha@gmail.com>
2022-01-06 19:57:53 +0200

    Update

--- Test.sh
+++ Test.sh
@@ -1,6 +1,7 @@
 # Script info
-echo "  Script created by Jaroshevskii"
-echo "    version: 1.0"
+echo "Simple repo puplish to giScript created by Jaroshevskii"
+echo "  Create by: Jaroshevskii"
+echo "  Version  : 1.0"
 echo ""
 
 # Repo status
