eeba9e5b0f796bf20284df2c239deff036c46f42
jaroshevskii <jaroshevskiisasha@gmail.com>
2022-01-06 19:31:50 +0200

    Update

---
+++ Test.sh
@@ -0,0 +1,15 @@
+# Script info
+echo "Script created by Jaroshevskii"
+echo "  version: 1.0"
+
+# Repo status
+git status
+
+# Add all repo changes
+git add --all
+
+# Create new commit
+git commit --message="Update"
+
+# Push all commis to github
+git push
